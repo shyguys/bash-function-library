4597db2697feb6928dd4de9375064f0143b1fb48
oed-neufeldm <martin.neufeld@oediv.de>
2022-10-29 20:18:43 +0000

    feat: element_in.sh

---
+++ scripts/element_in.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# Check if an element is in an array.
+
+element_in::display_usage() {
+  cat << EOL
+Usage: element_in <MATCH> <ARRAY> [OPTION]...
+Check if an element is in an array.
+
+Options:
+  tbc
+EOL
+}
+
+element_in() {
+  local SCRIPT_NAME
+  local GETOPT_OPTS
+  local GETOPT_LONGOPTS
+  local GETOPT_PARSED_ARGS
+  local GETOPT_RETURN_CODE
+
+  SCRIPT_NAME="$(basename "${0%.*}")"
+  GETOPT_LONGOPTS="help"
+  GETOPT_PARSED_ARGS="$(getopt -n "${SCRIPT_NAME}: element_in" -o "${GETOPT_OPTS}" -l "${GETOPT_LONGOPTS}" -- "$@")"
+  GETOPT_RETURN_CODE=$?
+  if [[ GETOPT_RETURN_CODE -ne 0 ]]; then
+    echo "${SCRIPT_NAME}: element_in: internal error."
+    exit 2
+  fi
+
+  local MATCH
+  local ELEMENT
+
+  eval set -- "${GETOPT_PARSED_ARGS}"
+  while true; do
+    case "${1}" in
+      "--")
+        MATCH="${2}"
+        shift 2
+        break
+      ;;
+    esac
+  done
+
+  for ELEMENT; do
+    if [[ "${ELEMENT}" == "${MATCH}" ]]; then
+      return 0
+    fi
+  done
+
+  return 1
+}
