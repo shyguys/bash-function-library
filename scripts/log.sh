6ae88fc1a323cb7c725bf7d59ba5a0a80a4cf8ca
oed-neufeldm <martin.neufeld@oediv.de>
2022-10-29 21:22:01 +0000

    feat: improve logging functionality

--- scripts/log.sh
+++ scripts/log.sh
@@ -8,7 +8,16 @@ Usage: log <MESSAGE> [OPTION]...
 Log messages to the command line.
 
 Options:
-  tbc
+  --bold       use bold mode.
+  --red        use red foreground color.
+  --green      use green foreground color.
+  --yellow     use yellow foreground color.
+  --blue       use blue foreground color.
+  --error      same as --bold --red.
+  --success    same as --bold --green.
+  --warning    same as --bold --yellow.
+  --info       same as --bold --blue.
+  --help       display this usage information.
 EOL
 }
 
@@ -20,7 +29,7 @@ log() {
   local GETOPT_RETURN_CODE
 
   SCRIPT_NAME="$(basename "${0%.*}")"
-  GETOPT_LONGOPTS="help,error,success,warning"
+  GETOPT_LONGOPTS="bold,red,green,yellow,blue,error,success,warning,info,help"
   GETOPT_PARSED_ARGS="$(getopt -n "${SCRIPT_NAME}: log" -o "${GETOPT_OPTS}" -l "${GETOPT_LONGOPTS}" -- "$@")"
   GETOPT_RETURN_CODE=$?
   if [[ GETOPT_RETURN_CODE -ne 0 ]]; then
@@ -28,41 +37,82 @@ log() {
     exit 2
   fi
 
-  local MESSAGE
-  local -i IS_ERROR
-  local -i IS_SUCCESS
-  local -i IS_WARNING
-  local ESCAPE_CODE_RESET
+  local ESCAPE_CODE_BOLD
   local ESCAPE_CODE_RED
   local ESCAPE_CODE_GREEN
   local ESCAPE_CODE_YELLOW
+  local ESCAPE_CODE_BLUE
+  local -i IS_BOLD
+  local -i IS_RED
+  local -i IS_GREEN
+  local -i IS_YELLOW
+  local -i IS_BLUE
+  local -a ESCAPE_CODES
+  local ESCAPE_CODE
+  local ESCAPE_SEQUENCE
+  local MESSAGE
 
-  ESCAPE_CODE_RESET="\033[0m"
-  ESCAPE_CODE_RED="\033[0;31m"
-  ESCAPE_CODE_GREEN="\033[0;32m"
-  ESCAPE_CODE_YELLOW="\033[0;33m"
+  ESCAPE_CODE_BOLD="1"
+  ESCAPE_CODE_RED="31"
+  ESCAPE_CODE_GREEN="32"
+  ESCAPE_CODE_YELLOW="33"
+  ESCAPE_CODE_BLUE="34"
 
   eval set -- "${GETOPT_PARSED_ARGS}"
   while true; do
     case "${1}" in
-      "--help")
-        log::display_usage
-        exit 0
+      "--bold")
+        IS_BOLD=1
+        shift 1
+      ;;
+
+      "--red")
+        IS_RED=1
+        shift 1
+      ;;
+
+      "--green")
+        IS_GREEN=1
+        shift 1
+      ;;
+
+      "--yellow")
+        IS_YELLOW=1
+        shift 1
+      ;;
+
+      "--blue")
+        IS_BLUE=1
+        shift 1
       ;;
 
       "--error")
-        IS_ERROR=1
+        IS_RED=1
+        IS_BOLD=1
         shift 1
       ;;
 
       "--success")
-        IS_SUCCESS=1
+        IS_GREEN=1
+        IS_BOLD=1
         shift 1
       ;;
 
       "--warning")
-        IS_WARNING=1
+        IS_YELLOW=1
+        IS_BOLD=1
         shift 1
+      ;;
+
+      "--info")
+        IS_BLUE=1
+        IS_BOLD=1
+        shift 1
+      ;;
+
+      "--help")
+        log::display_usage
+        exit 0
       ;;
 
       "--")
@@ -70,23 +120,35 @@ log() {
         shift 2
         break
       ;;
-    esac    
+    esac
   done
 
-  if [[ $IS_ERROR -eq 1 ]]; then
-    echo -e "${ESCAPE_CODE_RED}${SCRIPT_NAME}: ${MESSAGE}${ESCAPE_CODE_RESET}"
+  if [[ $IS_BOLD -eq 1 ]]; then
+    ESCAPE_CODES+="${ESCAPE_CODE_BOLD}"
+  fi
+
+  if [[ $IS_RED -eq 1 ]]; then
+    ESCAPE_CODES+=("${ESCAPE_CODE_RED}")
+  elif [[ $IS_YELLOW -eq 1 ]]; then
+    ESCAPE_CODES+=("${ESCAPE_CODE_YELLOW}")
+  elif [[ $IS_GREEN -eq 1 ]]; then
+    ESCAPE_CODES+=("${ESCAPE_CODE_GREEN}")
+  elif [[ $IS_BLUE -eq 1 ]]; then
+    ESCAPE_CODES+=("${ESCAPE_CODE_BLUE}")
+  fi
+
+  for ESCAPE_CODE in "${ESCAPE_CODES[@]}"; do
+    if [[ -n "${ESCAPE_SEQUENCE}" ]]; then
+      ESCAPE_SEQUENCE+=";"
+    fi
+
+    ESCAPE_SEQUENCE+="${ESCAPE_CODE}"
+  done
+
+  if [[ -z "${ESCAPE_SEQUENCE}" ]]; then
+    echo -e "${SCRIPT_NAME}: ${MESSAGE}"
     return 0
   fi
 
-  if [[ $IS_WARNING -eq 1 ]]; then
-    echo -e "${ESCAPE_CODE_YELLOW}${SCRIPT_NAME}: ${MESSAGE}${ESCAPE_CODE_RESET}"
-    return 0
-  fi
-
-  if [[ $IS_SUCCESS -eq 1 ]]; then
-    echo -e "${ESCAPE_CODE_GREEN}${SCRIPT_NAME}: ${MESSAGE}${ESCAPE_CODE_RESET}"
-    return 0
-  fi
-
-  echo "${SCRIPT_NAME}: ${MESSAGE}"
+  echo -e "\033[${ESCAPE_SEQUENCE}m${SCRIPT_NAME}: ${MESSAGE}\033[0m"
 }
