b008fa47006b234beb3d1f295e980ed2be680a93
oed-neufeldm <martin.neufeld@oediv.de>
2022-11-01 21:56:24 +0000

    feat/log: stdout_cache functions - maybe useful sometime

--- scripts/log.sh
+++ scripts/log.sh
@@ -2,6 +2,48 @@
 #
 # Log messages to the command line.
 
+# -------------------------------------- BEGIN STDOUT_CACHE -------------------------------------- #
+
+# declare STDOUT_CACHE_FILE
+
+# STDOUT_CACHE_FILE=""
+
+# trap_exit_on() {
+#   trap 'trap_exit' EXIT
+# }
+
+# trap_exit_off() {
+#   trap - EXIT
+# }
+
+# trap_exit() {
+#   rm -rf "$(dirname "${STDOUT_CACHE_FILE}")"
+# }
+
+# cache_stdout_on() {
+#   exec 3<> /dev/null
+#   exec 3>&1
+#   mkdir "$(dirname "${STDOUT_CACHE_FILE}")"
+#   exec 1> "${STDOUT_CACHE_FILE}"
+# }
+
+# cache_stdout_off() {
+#   exec 1>&3
+#   exec 3>&-
+# }
+
+# write_stdout_cache() {
+#   cat "${STDOUT_CACHE_FILE}"
+# }
+
+# flush_stdout_cache() {
+#   > "${STDOUT_CACHE_FILE}"
+# }
+
+# --------------------------------------- END STDOUT_CACHE --------------------------------------- #
+
+# ################################################################################################ #
+
 log::display_usage() {
   cat << EOL
 Usage: log <MESSAGE> [OPTION...]
